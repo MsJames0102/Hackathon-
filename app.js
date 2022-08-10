1e8597e8e59a245a111c66ee09d09ea6f0864e8c
Meka James <mekajames@Mekas-MBP.attlocal.net>
2022-08-10 11:23:27 -0500

    first commit

---
+++ app.js
@@ -0,0 +1,7 @@
+// var scene = document.querySelector('a-scene')
+
+// for(var i = 0; i < 500; i++) {
+//     var tree = document.createElement('a-entity')
+//     tree.setAttribute("green", green) // for obj-model, rotation, scale, position, whatever
+//     scene.addChild(tree)
+// }
